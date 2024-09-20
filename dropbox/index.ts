ded80dc86b59ba8a48af8af6dbc9bebbbc756b8c
albertoleandrosanchez <albertoleandrosanchez@hotmail.com>
2024-09-20 12:38:35 -0300

    Refactor .gitignore and update Dropbox configuration

--- dropbox/index.ts
+++ dropbox/index.ts
@@ -1,6 +1,5 @@
 import { Dropbox } from "dropbox";
 
-// export const dbx = new Dropbox({
-//   accessToken:
-//     "sl.BksMvmvwZtBiINigCB00GOMeZIXZxKB6R_2jUAQJCDUKrm34aSrMg7nO0_DfnwJMXzpLRwBosngji121k5jVk-ScW5KcxbGkKOn6nZzJJAIfWPZd_LYXwOxOpuhJAZN3nJnB0qevJcMnt62NqVve3U",
-// });
+export const dbx = new Dropbox({
+  accessToken: process.env.NEXT_PUBLIC_DROPBOX_ACCESS_TOKEN,
+});
